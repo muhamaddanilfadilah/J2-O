62b9d7cc6aa8c37fc2835a5a73653f6c5d2b1c2c
muhamaddanilfadilah <muhamaddanilfadilah@smkwikrama.sch.id>
2026-03-16 02:14:51 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+anchor.addEventListener("click", function(e) {
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+behavior:"smooth"
+});
+});
+});
+
+const images = document.querySelectorAll(".gallery-img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const close = document.getElementById("close");
+
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        lightbox.style.display = "flex";
+        lightboxImg.src = img.src;
+    });
+});
+
+close.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
+
+const cards = document.querySelectorAll(".card, .member-card, .game-card");
+
+window.addEventListener("scroll", () => {
+
+cards.forEach(card => {
+
+const top = card.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 50){
+card.style.opacity = "1";
+card.style.transform = "translateY(0)";
+}
+
+});
+
+});
